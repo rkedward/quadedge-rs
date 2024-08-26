0c3dc42ee273779863c0b548b2d3edecb2a9b262
Russ Edwards <russell.k.edwards@gmail.com>
2024-08-26 09:50:47 -0600

    Major refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,112 +1,66 @@
 #![allow(dead_code)]
 use std::cell::Cell;
-use std::fmt::{self, Debug, Formatter};
-use std::ops::Index;
 use typed_arena::Arena;
 
-#[derive(Default)]
-pub struct Edge<'m, D> {
-    refs: Cell<Option<[&'m Edge<'m, D>; 2]>>,
-    data: Cell<Option<&'m D>>,
-}
+type EdgeRef<'m, V, F> = (&'m QuadEdge<'m, V, F>, u8);
+type EdgeRefCell<'m, V, F> = Cell<Option<EdgeRef<'m, V, F>>>;
+type VertCell<'m, V> = Cell<Option<&'m V>>;
+type FaceCell<'m, F> = Cell<Option<&'m F>>;
 
-impl<'m, D> Edge<'m, D> {
-    #[inline]
-    fn next(&self) -> &'m Edge<'m, D> {
-        self.refs.get().unwrap()[0]
-    }
-
-    #[inline]
-    fn rot(&self) -> &'m Edge<'m, D> {
-        self.refs.get().unwrap()[1]
-    }
-
-    #[inline]
-    pub fn set(&self, val: &'m D) {
-        self.data.set(Some(val));
-    }
-
-    #[inline]
-    pub fn splice(&self, other: &'m Edge<'m, D>) {
-        let _alpha = self.next().rot();
-        let _beta = other.next().rot();
-        // todo - impliment splice operator
-    }
-
-
-}
-
-impl<'m, D> PartialEq for Edge<'m, D> {
-    fn eq(&self, other: &Self) -> bool {
-        std::ptr::eq(self, other)
-    }
-}
-
-impl<'m, D> Debug for Edge<'m, D> {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "Edge({:#08x?})", self as *const _)
-    }
-}
 
 #[derive(Default, Debug)]
-pub struct QuadEdge<'m, D> {
-    edges: [Edge<'m, D>; 4],
+pub struct QuadEdge<'m, V, F> {
+    next: [EdgeRefCell<'m, V, F>; 4],
+    data: (VertCell<'m, V>, FaceCell<'m, F>, VertCell<'m, V>, FaceCell<'m, F>),
 }
 
-impl<'m, D> QuadEdge<'m, D> {
-    fn init(&'m self, edges: [&'m Edge<'m, D>; 4]) {
-        for ind in 0..=3 {
-            self.edges[ind].refs.set(Some([edges[ind], &self[ind + 1]]));
-        }
-    }
-
-    fn set(&self, vals: [&'m D; 4]) {
-        for ind in 0..=3 {
-            self.edges[ind].set(vals[ind]);
-        }
+impl<'m, V, F> QuadEdge<'m, V, F> {
+    #[inline]
+    fn onext(&'m self, r: u8) -> EdgeRef<'m, V, F> {
+        self.next[(r + 1).rem_euclid(4) as usize].get().unwrap()
     }
 
     #[inline]
-    fn onext(&self) -> &'m Edge<'m, D> {
-        self.edges[0].next()
+    fn rot(&'m self, r: u8) -> EdgeRef<'m, V, F> {
+        (self, (r + 1).rem_euclid(4))
     }
 }
 
-impl<'m, D> PartialEq for QuadEdge<'m, D> {
+impl<'m, V, F> PartialEq for QuadEdge<'m, V, F> {
     fn eq(&self, other: &Self) -> bool {
         std::ptr::eq(self, other)
     }
 }
 
-impl<'m, D> Index<usize> for QuadEdge<'m, D> {
-    type Output = Edge<'m, D>;
-
-    fn index(&self, ind: usize) -> &Self::Output {
-        &self.edges[(ind).rem_euclid(4)]
-    }
+#[derive(Default)]
+pub struct Manifold<'m, V, F> {
+    quads: Arena<QuadEdge<'m, V, F>>,
+    verts: Arena<V>,
+    faces: Arena<F>,
 }
 
-pub struct Manifold<'m, D> {
-    quads: Arena<QuadEdge<'m, D>>,
-    data: Arena<D>,
-}
-
-impl<'m, D: Default> Manifold<'m, D> {
-    pub fn new() -> Self {
-        Self {
-            quads: Arena::new(),
-            data: Arena::new(),
-        }
-    }
-
-    pub fn make_quad(&'m self) -> &'m QuadEdge<'m, D> {
+impl<'m, V: Default + Copy, F: Default + Copy> Manifold<'m, V, F> {
+    pub fn make_quad(&'m self) -> &'m QuadEdge<'m, V, F> {
         let q = self.quads.alloc(QuadEdge::default());
-        q.init([&q[0], &q[3], &q[2], &q[1]]);
+        q.next[0].set(Some((q, 0)));
+        q.next[1].set(Some((q, 3)));
+        q.next[2].set(Some((q, 2)));
+        q.next[3].set(Some((q, 1)));
         q
     }
 
-    pub fn make_datum(&self, val: D) -> &mut D {
-        self.data.alloc(val)
+    pub fn splice(&'m self, a: EdgeRef<'m, V, F>, b: EdgeRef<'m, V, F>) {
+        if a.1.rem_euclid(2) != b.1.rem_euclid(2) {
+            panic!("Incompatible edge types!")
+        }
+
+        let (c, i) = a.0.onext(a.1);
+        let _alpha = c.rot(i);
+
+        let (d, i) = b.0.onext(b.1);
+        let _beta = d.rot(i);
+
+
     }
 }
 
@@ -114,37 +68,41 @@ impl<'m, D: Default> Manifold<'m, D> {
 mod tests {
     use super::*;
 
-    #[derive(Default)]
-    struct Datum;
+    #[test]
+    fn check_u8_rem_euclid_4() {
+        assert_eq!(0u8.rem_euclid(4), 0);
+        assert_eq!(1u8.rem_euclid(4), 1);
+        assert_eq!(2u8.rem_euclid(4), 2);
+        assert_eq!(3u8.rem_euclid(4), 3);
+        assert_eq!(4u8.rem_euclid(4), 0);
+        assert_eq!(8u8.rem_euclid(4), 0);
+        assert_eq!(10u8.rem_euclid(4), 2);
+    }
 
     #[test]
     fn check_internal_operations() {
-        let m: Manifold<()> = Manifold::new();
+        let m: Manifold<(), ()> = Manifold::default();
         let q = m.make_quad();
-        assert_eq!(q[0].next(), &q[0]);
-        assert_eq!(q[1].next(), &q[3]);
-        assert_eq!(q[2].next(), &q[2]);
-        assert_eq!(q[3].next(), &q[1]);
-        assert_eq!(q[4].next(), &q[0]);
-        assert_eq!(q[0].rot(), &q[1]);
-        assert_eq!(q[1].rot(), &q[2]);
-        assert_eq!(q[2].rot(), &q[3]);
-        assert_eq!(q[3].rot(), &q[0]);
-        assert_eq!(q[0].rot().next(), &q[3]);
-        assert_eq!(q[1].next().rot(), &q[0]);
-        assert_eq!(q.onext(), q[0].next());
+        assert_eq!(q.onext(0), (q, 0));
+        assert_eq!(q.onext(1), (q, 3));
+        assert_eq!(q.onext(2), (q, 2));
+        assert_eq!(q.onext(3), (q, 1));
+        assert_eq!(q.onext(4), (q, 0));
+
+        assert_eq!(q.rot(0), (q, 1));
+        assert_eq!(q.rot(1), (q, 2));
+        assert_eq!(q.rot(2), (q, 3));
+        assert_eq!(q.rot(3), (q, 0));
+        assert_eq!(q.rot(4), (q, 1));
     }
 
     #[test]
-    fn check_splice_operations() { 
-        let m: Manifold<()> = Manifold::new();
+    fn check_splice_operations() {
+        let m: Manifold<(), ()> = Manifold::default();
         let q0 = m.make_quad();
         let q1 = m.make_quad();
-        q0[0].splice(&q1[1]);
-        q0[0].splice(&q1[1]);
+        m.splice((q0, 1), (q1, 3));
 
-        // TodDo: Add assert statememts
+        // TodDo: Add assert statements
     }
-
-
 }
--- src/main.rs
+++
@@ -1,7 +0,0 @@
-use quadedge_rs::Manifold;
-
-fn main() {
-    let manifold: Manifold<()> = Manifold::new();
-    let q = manifold.make_quad();
-    println!("{:?}", q);
-}
